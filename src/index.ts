4b94ade8c5f18735e15d90682da0582dbe19a528
preciouskayili <preciouskayili@gmail.com>
2024-08-19 00:48:49 +0100

    Debugging cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ PeerServer({ port: 9000, path: `${process.env.API_VERSION}/peerjs` });
 app.use(
   `/${process.env.API_VERSION}/*`,
   cors({
-    origin: process.env.CLIENT_ORIGIN!,
+    origin: ["*"],
     allowHeaders: ["Authorization", "Content-Type"],
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     maxAge: 86400,
